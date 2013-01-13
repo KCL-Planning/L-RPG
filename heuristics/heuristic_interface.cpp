c2ebbb109e254e76d2645f50023e08e8ec8777a9
Morloth <Morloth@17c1eecd-4a2e-456d-aae0-6c21ee981012>
2013-01-13 16:27:14 +0000

    Add source file of heuristic interface.

---
+++ heuristics/heuristic_interface.cpp
@@ -0,0 +1,22 @@
+#ifndef MYPOP_HEURISTICS_HEURISTIC_INTERFACE_H
+#define MYPOP_HEURISTICS_HEURISTIC_INTERFACE_H
+
+#include "heuristic_interface.h"
+
+namespace MyPOP {
+
+class GroundedAtom;
+class State;
+
+namespace HEURISTICS {
+
+HeuristicInterface::~HeuristicInterface()
+{
+	
+}
+
+};
+
+};
+
+#endif
